d0a7d608a9eb23d88c34da8c68e6a274c7c40a86
Tatiana Turin <tatiana.turin@gmail.com>
2022-06-30 11:39:03 -0400

    commit

--- nine_lives_updated.py
+++ nine_lives_updated.py
@@ -6,11 +6,25 @@ level 2 - word length 6 -9
 level 3 -word length 10 and up
 
 making the game smarter
-the game will end once the last letter is typed in
+the game will end once the last letter is typed in -> this will fail if the person inputs the same correct letter twice
+
+if a letter was already guessed and the user enters it again, catch this
+str(clue).__contains__(guessed_letter) == False:
+
 
 """
 import random
+######################################################################
+def update_clue(guessed_letter, secret_word, clue, hidden_letters): #function that replaces the guessed letter in the secret word
+    index = 0
+    while index < len(secret_word):
+        if guessed_letter == secret_word[index] and str(clue).__contains__(guessed_letter) == False:
+            clue[index] = guessed_letter
+            hidden_letters -= 1 ## one less hidden letter remains
 
+        index += 1
+    return hidden_letters # returns an integer when function is called3
+########################################################################
 lives = 9
 words = ['cat','syndrome','matrix','thriftless','galaxy','megahertz','luxury','brooklyn','curacao','xylophone','mississippi']
 secret_word = random.choice(words) # uses random module's choice() function
@@ -20,7 +34,7 @@ guessed_word_correctly = False
 difficulty = input('Choose your level of difficulty: Enter 1 (beginner), 2 (intermediate) and 3 (expert)')
 new_words = list()
 
-for x in words:
+for x in words: #set difficulty level
     if difficulty=='1' and len(x)<6:
         new_words.append(x)
         lives = 5
@@ -31,36 +45,24 @@ for x in words:
         new_words.append(x)
         lives = 13
 secret_word = random.choice(new_words)
-
+print(secret_word)
 remaining_letters = len(secret_word)
-
-
-def update_clue(guessed_letter, secret_word, clue, hidden_letters):
-    index = 0
-    while index < len(secret_word):
-        if guessed_letter == secret_word[index]:
-            clue[index] = guessed_letter
-            hidden_letters -= 1 ## one less hidden letter remains
-        index += 1
-    return hidden_letters # returns an integer when function is called3
-#############################################################################################################
-
-
-
+print('Remaining letters '+ str(remaining_letters))
 for i in secret_word: ##populate clue list with question mark
     clue.append('?')
 
 while lives > 0:
     print(clue)
     print('Lives left: ' + heart_symbol * lives)
-    guess = input('Guess a letter or the whole word ')
+    guess = input('Guess a letter or the whole word\n ')
 
-    if guess == secret_word:
+    if guess == secret_word: #if you type in the entire word correctly
         guessed_word_correctly = True
         break
 
-    if guess in secret_word:
-        remaining_letters=update_clue(guess,secret_word,clue,remaining_letters)
+    if guess in secret_word: #if the input is a letter in the secret word
+        remaining_letters=update_clue(guess,secret_word,clue,remaining_letters) # call function that
+        print(remaining_letters)
     else:
         print('Incorrect. You lose a life')
         lives -= 1
